2aef77df724c0a3298a23232ad03f7b9b76a273e
Alice Hepburn <aliceokhepburn@gmail.com>
2023-06-12 17:41:12 -0400

    tests passed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs([42 - someValue])
+  }
+
+  function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    return 264 * Math.abs(destination - start)
+  }
+
+  function calculatesFarePrice (start, destination) {
+    const feet = distanceTravelledInFeet(start, destination)
+    if (feet < 400) {
+        return 0
+    }
+    else if (feet < 2000) {
+        return (feet - 400) * (2 / 100)
+    } else if (feet >= 2500) {
+        return `cannot travel that far`
+    }
+    else {
+        return 25
+    }
+}
+  
